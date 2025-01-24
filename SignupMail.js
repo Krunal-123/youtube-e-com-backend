932c5bb8f52966ba1304e7d4b05871fe71470597
Krunal-Parmar <100572810+Krunal-123@users.noreply.github.com>
2025-01-24 17:32:53 +0530

    3.1

--- SignupMail.js
+++ SignupMail.js
@@ -14,8 +14,8 @@ const signupMail = (email,firstName,lastName) => {
     to: email,
     subject: 'Welcome to Our Platform!',
     html: `
-  <h2>Dear ${firstName} ${lastName},</h2>
-  <img scr="https://media.discordapp.net/attachments/1116972017883815996/1283360451689844768/WhatsApp_Image_2024-09-10_at_12.59.07_50c89b17.jpg?ex=66e2b60f&is=66e1648f&hm=f1123c7604906933c9cce518f1bc73edd22a427b3aea8e91444a313a0f2f42c3&=&format=webp&width=782&height=521" style="height:200px width:100%">
+  <h2>Dear ${firstName},</h2>
+  <img scr="https://i.ytimg.com/vi/_knbWxxFuz8/maxresdefault.jpg" style="height:200px width:100%">
   <p>
   Welcome to Your our own E Com Website! We’re thrilled to have you join our community. Whether you’re here to [mention key feature or benefit of the site], explore our resources, or connect with like-minded individuals, we’re here to support you every step of the way.
 </p>
